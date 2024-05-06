c0c69b6bb97afdff32374bb640840328db7f1997
Isabelle <isabelle.torrezan@senacsp.edu.br>
2024-05-06 16:47:06 -0300

    lista de contato
    finalização do projeto da lista de contato

--- ListaDeContatosIsa/Contato.cs
+++ ListaDeContatosIsa/Contato.cs
@@ -43,16 +43,35 @@ namespace ListaDeContatosIsa
             }
             set
             {
-                if (value.Length == 13)
+                if (value.Length == 11)
                 {
                     telefone = value;
                 }
                 else
                 {
-                    telefone = "(00) 00000-0000";
+                    telefone = "00 00000-0000";
                 }
-
             }
+        }
+        public Contato()
+        {
+            Nome = "Amanda";
+            Sobrenome = "Souza";
+            Telefone = "(11) 98888-5555";
+        }
+        public Contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+        public override string ToString()
+        {
+            string saida = string.Empty;
+            saida += String.Format("{0} {1}", Nome, Sobrenome);
+            saida += String.Format("({0}) {1}-{2}", Telefone.Substring(0, 2), Telefone.Substring(3, 5), Telefone.Substring(7, 4));
+            return saida;
         }
     }
 }
+
--- ListaDeContatosIsa/Form1.cs
+++ ListaDeContatosIsa/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace ListaDeContatosIsa
 {
@@ -15,6 +17,104 @@ namespace ListaDeContatosIsa
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private Contato[] listaDeContatos = new Contato[1];
+
+        private void Escrever(Contato contato)
+        {
+            StreamWriter escrever = new StreamWriter("Contatos.txt");
+
+            escrever.WriteLine(listaDeContatos.Length + 1);
+            escrever.WriteLine(contato.Nome);
+            escrever.WriteLine(contato.Sobrenome);
+            escrever.WriteLine(contato.Telefone);
+
+            for (int x = 0; x < listaDeContatos.Length; x++)
+            {
+                escrever.WriteLine(listaDeContatos[x].Nome);
+                escrever.WriteLine(listaDeContatos[x].Sobrenome);
+                escrever.WriteLine(listaDeContatos[x].Telefone);
+            }
+
+            escrever.Close();
+        }
+
+
+        private void Ler()
+        {
+            StreamReader ler = new StreamReader("Contatos.txt");
+            listaDeContatos = new Contato[Convert.ToInt32(ler.ReadLine())];
+
+            for (int x = 0; x < listaDeContatos.Length; x++)
+            {
+                listaDeContatos[x] = new Contato();
+                listaDeContatos[x].Nome = ler.ReadLine();
+                listaDeContatos[x].Sobrenome = ler.ReadLine();
+                listaDeContatos[x].Telefone = ler.ReadLine();
+            }
+            ler.Close();
+        }
+
+        private void Exibir()
+        {
+            listBoxContatos.Items.Clear();
+
+            for (int x = 0; x < listaDeContatos.Length; x++)
+            {
+                listBoxContatos.Items.Add(listaDeContatos[x].ToString());
+            }
+
+        }
+
+        private void LimparCampos()
+        {
+            textBoxNome.Text = String.Empty;
+            textBoxSobrenome.Text = String.Empty;
+            textBoxTelefone.Text = String.Empty;
+        }
+
+        private void buttonIncluirContato_Click(object sender, EventArgs e)
+        {
+            Contato contato = new Contato();
+
+            listBoxContatos.Items.Add(contato.ToString());
+
+            Escrever(contato);
+            Ler();
+            Exibir();
+            LimparCampos();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sContato contato = new Contato();
+
+            contato.Nome = textBoxNome.Text;
+            contato.Sobrenome = textBoxNome.Text;
+            contato.Telefone = textBoxNome.Text;
+
+            listBoxContatos.Items.Add(contato.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ler();
+            Exibir();
         }
     }
 }
